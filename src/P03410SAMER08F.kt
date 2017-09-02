1950aff3982df3b757b3b73ef37b6d87f90a91f1
alvinrxg <alvin.rxg@gmail.com>
2017-09-02 13:20:31 +0200

    add 3410 Feynman

---
+++ src/P03410SAMER08F.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(args: Array<String>) {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    reader.readLines().map { it.toInt() }.takeWhile { it != 0 }.forEach {
+        (1..it).fold(0) { result, v ->
+            result + v * v
+        }.printOut()
+    }
+}
+
+fun Int.printOut() = println(this)
